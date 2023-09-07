2fc60142958c744b89c974a382b653646bf191a3
natalikazakova <natalikazakova87@gmail.com>
2023-09-07 21:56:45 +0700

    initial commit

--- src/test/java/page/DashBoardPage.java
+++ src/test/java/page/DashBoardPage.java
@@ -5,7 +5,7 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import data.DataHelper;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Condition.text;
